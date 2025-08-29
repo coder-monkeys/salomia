9982cbabe95e1d1ad863e90078c1f7985d07f09f
Dravid92 <dravidhemanth18@gmail.com>
2025-08-29 23:42:30 +0530

    [wall] added text position dynamic and json format update

--- build_video.py
+++ build_video.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import List
+from typing import List, Dict, Any
 
 
-def build_ffmpeg_command(template_json: object, output_path: Path) -> List[str]:
+def build_ffmpeg_command(template_json: Dict[str, Any], output_path: Path) -> List[str]:
     """
     Build the ffmpeg command for creating a slideshow video.
     Args:
@@ -16,16 +16,19 @@ def build_ffmpeg_command(template_json: object, output_path: Path) -> List[str]:
     images = template_json['images']
     videos = template_json['videos']
     audio_path = template_json['audio']
+    dimension_w = template_json['width']
+    dimension_h = template_json['height']
     for i, slide in enumerate(images):
+        text_attr = slide['attributes']['text'] if 'text' in slide['attributes'] else None
         inputs.extend(["-loop", "1", "-t", str(slide["duration"]), "-i", slide["image"]])
-        safe_text = slide['text'].replace("'", r"\'").replace(':', r'\:')
-
-        filter_parts.append(
-            f"[{i}:v]scale=1080:1350,setsar=1,"
-            f"drawtext=text='{safe_text}':fontcolor='{slide['fontcolor']}':fontsize={slide['fontsize']}:"
-            f"x=(w-text_w)/2:y=h-100[slide{i}]"
-        )
-
+        filter_part_str = f"[{i}:v]scale={dimension_w}:{dimension_h},setsar=1,"
+        if text_attr:
+            text_position_x = text_attr['x']
+            text_position_y = text_attr['y']
+            safe_text = text_attr['content'].replace("'", r"\'").replace(':', r'\:')
+            filter_part_str += f"drawtext=text='{safe_text}':fontcolor='{text_attr['fontcolor']}':fontsize={text_attr['fontsize']}:" \
+                               f"x={text_position_x}:y={text_position_y}[slide{i}]"
+        filter_parts.append(filter_part_str)
     concat_inputs = "".join(f"[slide{i}]" for i in range(len(images)))
     filter_complex = ";".join(filter_parts) + f";{concat_inputs}concat=n={len(images)}:v=1:a=0[outv]"
 
